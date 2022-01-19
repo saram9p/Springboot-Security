40cc714e1fe1341bc2b76c96412c5b81fbd2dc8e
saram <saram9p@gmail.com>
2022-01-19 23:12:16 +0900

    회원가입

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.cos.security1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //@Configuration  // 메모리에 뜨게함
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
@@ -18,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().permitAll() // 위의 세개 주소가 아닌 주소는 다 권한이 허용이 된다.
 			.and()
 			.formLogin()
-			.loginPage("/login");
+			.loginPage("/loginForm");
 
 	}
 }
--- src/main/java/com/cos/security1/controller/indexController.java
+++ src/main/java/com/cos/security1/controller/indexController.java
@@ -1,12 +1,24 @@
 package com.cos.security1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
 
 @Controller // View를 리턴하겠다.
 public class indexController {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	// localhost:8080/
 	// localhost:8080
 	@GetMapping({"", "/"})
@@ -33,18 +45,24 @@ public class indexController {
 	}
 	
 	// 스프링시큐리티 해당주소를 낚아채버림 - SecurityConfig 파일 생성 후 작동안함.
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
 	}
 	
-	@GetMapping("/joinProc")
-	public @ResponseBody String joinProc() {
-		return "회원가입 완료됨";
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user);
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user); // 회원가입 잘됨. 비밀번호 : 1234 => 시큐리티로 로그인을 할 수 없음, 이유는 패스워드가 암호화가 안 되었기 때문에 
+		return "redirect:/loginForm"; // redirect를 붙이면 /loginForm 이라는 함수를 호출
 	}
 }
