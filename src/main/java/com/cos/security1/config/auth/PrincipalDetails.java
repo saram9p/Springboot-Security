22a425f74806f065d2eb7db2c07f30dff823ca80
saram9p <saram9p@gmail.com>
2022-03-14 22:20:49 +0900

    Authentication 객체가 가질수 있는 2가지 타입

--- src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -2,11 +2,15 @@ package com.cos.security1.config.auth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.cos.security1.model.User;
+
+import lombok.Data;
 
 // 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
 // 로그인을 진행이 완료가 되면 시큐리티 session을 만들어줍니다. (Security ContextHolder)
@@ -16,7 +20,9 @@ import com.cos.security1.model.User;
 
 // Security Session => Authentication => UserDetails(PrincipalDetails)
 
-public class PrincipalDetails implements UserDetails {
+@SuppressWarnings("serial")
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
 	private User user; //콤포지션
 	
@@ -71,6 +77,18 @@ public class PrincipalDetails implements UserDetails {
 		// 우리 사이트!! 1년동안 회원이 로그인을 안하면!! 휴먼 계정으로 하기로 함.
 		// 현재시간 - 로그인시간 => 1년을 초과하면 return false;
 		return true;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
